8e44625edc5e7efc4fac73bb329153af7cacc336
zhaozuyin <1515503374@qq.com>
2020-10-13 09:20:56 +0800

    Updates

---
+++ precache-manifest.134137ea0bec08522d9a975a1ad70bce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5765f253db72092610c1c21b6d4220c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07236a67a3b932b9b17a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d85956b26a72425b85c0",
+    "url": "/static/js/2.a2d67585.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2d67585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07236a67a3b932b9b17a",
+    "url": "/static/js/main.7a820c1c.chunk.js"
+  },
+  {
+    "revision": "fd54b029b7d19b595f84",
+    "url": "/static/js/runtime-main.f53b2653.js"
+  }
+]);
